ad568d5347fdf9ba674642e7b92e0f43bf26ed26
waltzfordebby <lslestercayabyab@gmail.com>
2018-12-17 15:26:55 +0800

    Initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCJZnZ6cKswh1QIVpZUHlgRtKYnkK7VB90",
+    authDomain: "vuefs-prod-f32b7.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-f32b7.firebaseio.com",
+    projectId: "vuefs-prod-f32b7",
+    storageBucket: "vuefs-prod-f32b7.appspot.com",
+    messagingSenderId: "257433278569"
+}
